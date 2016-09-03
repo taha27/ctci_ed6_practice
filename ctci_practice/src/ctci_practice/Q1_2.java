75aef8c85fcb48396c610b0c3723ee4658ccc6fa
taha27 <tahashabbir27@gmail.com>
2016-09-03 12:48:23 -0400

    added q1.2 and q1.3

---
+++ ctci_practice/src/ctci_practice/Q1_2.java
@@ -0,0 +1,53 @@
+package ctci_practice;
+
+public class Q1_2 {
+
+	public static boolean is_permutation(String a, String b) {
+		
+		//a and b can only be permutations of the 
+		//other if they have the same size
+		if(a.length() != b.length()) return false;
+		
+		int[] letter_count_a = new int[26];
+		int[] letter_count_b = new int[26];
+		
+		for(int i = 0; i < a.length(); i++) {
+			
+			//increase the array value at index corresponding to the letter found
+			int letter_val_a = a.charAt(i) - 'a';
+			letter_count_a[letter_val_a]++;
+			
+			//increase the array value at index corresponding to the letter found
+			int letter_val_b = b.charAt(i) - 'a';
+			letter_count_b[letter_val_b]++;
+		}
+		
+		//compare the letter counts of both strings
+		for(int i = 0; i < 26; i++) {
+			if(letter_count_a[i] != letter_count_b[i]) return false;
+		}
+		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		//test1
+		String[] a = {"perm", "mop", "dog", "nice", "comeon"};
+		String[] b = {"marp", "mope", "god", "nice", "onmoce"};
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println("Are the words " + a[i] + " and " + b[i] + " permutations : " + is_permutation(a[i], b[i]));
+		}
+		
+		//test2
+		String[][] pairs = {{"apple", "papel"}, {"carrot", "tarroc"}, {"hello", "llloh"}};
+		for (String[] pair : pairs) {
+			String word1 = pair[0];
+			String word2 = pair[1];
+			boolean anagram = is_permutation(word1, word2);
+			System.out.println(word1 + ", " + word2 + ": " + anagram);
+		}
+	}
+
+}
---
+++ ctci_practice/src/ctci_practice/Q1_3.java
@@ -0,0 +1,68 @@
+package ctci_practice;
+
+public class Q1_3 {
+	
+	public static String replace_spaces(char[] url, int url_length) {
+		
+		int space_count = 0;
+		
+		//count the number of spaces in the string
+		for(int i =  0; i < url_length; i++) {
+			if(url[i] == ' ') space_count++;
+		}
+		
+		System.out.println("Space count : " + space_count);
+		
+		//calculate the length of the url including the replacement text
+		//url_length includes the length of the str and the spaces(to be replaced by %)
+		//space_count*2 represents the additional space needed to insert
+		//the second and third replacement characters (20)
+		int final_length = url_length + space_count * 2;
+		
+		//new char array to store the urlified text
+		char[] final_url = new char[final_length];
+		
+		//if(url_length < url.length) url[url_length] = '\0';
+		
+		for(int i =  url_length-1; i >= 0; i--) {
+			//replace the spaces with the url text
+			if(url[i] == ' ') {
+				final_url[final_length - 1] = '0';
+				final_url[final_length - 2] = '2';
+				final_url[final_length - 3] = '%';
+				final_length = final_length - 3;
+			} else {
+				final_url[final_length - 1] = url[i];
+				final_length--;
+			}
+			
+		}
+		
+		//change the char array back to a string
+		String final_str = new String(final_url);
+		
+		return final_str;
+	}
+	
+	public static int get_length(char[] url) {
+		
+		for(int i = url.length-1; i >= 0; i--) {
+			if(url[i] != ' ') {
+				//length of the url is one more than the index of the last char
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static void main(String[] args) {
+		
+		String str = "Mr Taha Shabbir";
+		char[] char_url = str.toCharArray();
+		
+		int url_len = get_length(char_url) + 1;
+		
+		System.out.println(str + " : " + replace_spaces(char_url, url_len));
+	}
+
+}
